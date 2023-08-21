729350bb88366222e9ddbc1f6d954c2d01843ed9
Heetraval <heetraval26@gmail.com>
2023-08-21 12:48:04 +0530

    abc

--- 4.py
+++ 4.py
@@ -1,2 +1,10 @@
-for i in range(1,100,1):
-    print(i+1)
+for i in range(15):
+    print("upto i", i+1)
+    if(i==4):
+        break
+print("***")
+    
+for i in range(10):
+    if(i == 8):
+        continue
+    print("upto i", i+1)
