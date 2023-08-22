d45dacebfbc7339aeeb4180cfd758dec29c0e3c9
Heetraval <heetraval26@gmail.com>
2023-08-22 12:38:28 +0530

    newwcomit

--- 22.py
+++ 22.py
@@ -3,3 +3,19 @@ class user:
         self.__name = "Hardee"
 a = user()
 print(a._user__name)
+
+class user2:
+    def __init__(self):
+        self._name = "Hardee"
+    def _func1(self):
+        return "codewithhardee"
+    
+class user3(user2):
+    pass
+ 
+a1 = user3()
+a2 = user3()
+print(a1._name)
+print(a1._func1())
+print(a2._name)
+print(a2._func1())
